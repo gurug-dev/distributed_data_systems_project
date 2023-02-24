f02e1ac89c4d55a866ea4fc07afc78c591cc69bb
Dura-Europos <zhangyoushi1996@gmail.com>
2023-02-24 02:13:27 -0800

    add reddit calls to task file

--- msds697_task2.py
+++ msds697_task2.py
@@ -7,6 +7,7 @@ from airflow.operators.python import PythonOperator
 from user_definition import *
 
 from financial_fillings_scrape import *
+from reddit_calls import *
 # NOTE : In order to make sure it send configurations requests first, do not import your .py reading from gs.
 
 
@@ -14,12 +15,16 @@ def _download_financial_filings_data():
     blob_name = f'{folder_name}/{financial_file_name}.csv'
     df = scrape_and_return_financials_df()
     write_csv_to_gcs(bucket_name, blob_name, service_account_key_file, df)
-    
-#TODO: reddit data pull
-# def _download_sf_weather_data():
-#     data = filter_history_data(retreive_api_data(noaa_token, noaa_api_url),
-#                                three_days_ago)  
-#     write_json_to_gcs(bucket_name, f'{three_days_ago}/weather.json', service_account_key_file, data)
+
+# reddit data pull by Youshi
+def _download_reddit_data():
+    """wrapper function for all the steps
+    for testing only
+    """
+    for subreddit in list_of_reddits:
+        blob_name = f'{folder_name}/{subreddit}.csv'
+        df = retrive_7days_reddit_posts(subreddit, folder_name)
+        write_reddit_data_gcb(bucket_name, blob_name, service_account_key_file, df)
     
 
 with DAG(
@@ -27,7 +32,7 @@ with DAG(
     schedule=None,
     start_date=datetime(2023, 1, 1),
     catchup=False,
-    schedule_interval='@daily'
+    schedule_interval='@daily' # reddit data is planned to be called weekly, change function schedule?
 ) as dag:
 
 #     create_insert_aggregate = SparkSubmitOperator(
@@ -48,10 +53,10 @@ with DAG(
                                                   python_callable = _download_financial_filings_data,
                                                   dag=dag)
 
-#TODO
-#     download_reddit_data = PythonOperator(task_id = "download_reddit_data",
-#                                                     python_callable = _download_reddit_data,
-#                                                     dag=dag)
+    download_reddit_data = PythonOperator(task_id = "download_reddit_data",
+                                                    python_callable = _download_reddit_data,
+                                                    dag=dag)
     download_financial_filing_data
-#     download_reddit_data >> create_insert_aggregate
+    download_reddit_data 
+    ##>> create_insert_aggregate
 
