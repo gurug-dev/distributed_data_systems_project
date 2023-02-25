9183dfcdf887b241e44294008e84281d49600cc2
Gurusankar Gopalakrishnan <gurug@zephyr.local>
2023-02-24 19:03:32 -0800

    removing comments

--- user_definition.py
+++ user_definition.py
@@ -26,9 +26,6 @@ collection_name_reddit = "reddit"
 REF_STRING = "mongodb+srv://admin:<password>@msds697-cluster.qzgwq.mongodb.net/"
 MONGO_DB_NAME = "msds697_project"
         
-        
-# folder_name = str(date.today())
-
 # variables associated with reddit call
 yesterday = datetime.today() - timedelta(days=1)
 yesterday_str = yesterday.strftime("%Y-%m-%d")
@@ -42,24 +39,4 @@ list_of_reddits = ['wallstreetbets','stocks','pennystocks','DueDiligence','small
                    'RobinHood']
 
 # list_of_reddits = ['shroomstocks']
-
-# # two_days_ago = date.today() - timedelta(days=4)
-# # three_days_ago = date.today() - timedelta(days=5) 
-# two_days_ago = '2023-01-26'
-# three_days_ago = '2023-01-25'
-
-# sf_data_url = "data.sfgov.org"
-# data_limit = 200000
-# sf_data_sub_uri = "gnap-fj3t"
-# sf_data_app_token = os.environ.get("SF_DATA_TOKEN")
-
-# noaa_token = os.environ.get("NOAA_TOKEN")
-# # station_id = "GHCND:USW00023272"
-# # dataset_id = "GHCND"
-# # location_id = "CITY:US060031"
-# noaa_endpoint = f"data?datasetid={dataset_id}&datatypeid=PRCP&station_id={station_id}&startdate={three_days_ago}&enddate={two_days_ago}"
-# noaa_api_url = f"https://www.ncei.noaa.gov/cdo-web/api/v2/{noaa_endpoint}"
-
-# bucket_name = os.environ.get("GS_BUCKET_NAME")
-# service_account_key_file = os.environ.get("GS_SERVICE_ACCOUNT_KEY_FILE")
 
