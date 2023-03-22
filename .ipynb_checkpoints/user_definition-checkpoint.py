3373c36aecd535b13bdde8ee7da62ffe3a37cc05
Gurusankar Gopalakrishnan <gurug@zephyr.local>
2023-03-22 16:18:02 -0700

    cleanup

--- .ipynb_checkpoints/user_definition-checkpoint.py
+++ .ipynb_checkpoints/user_definition-checkpoint.py
@@ -17,42 +17,26 @@ paths_to_clear = ["./datasets/INDICES","./datasets/RAW_FILINGS","./datasets/FILI
 mongo_username = os.environ.get("MONGO_USERNAME")
 mongo_password =  os.environ.get("MONGO_PASSWORD")
 mongo_ip_address = os.environ.get("MONGO_IP")
+# mongo_ip_address = 'localhost'
+
 database_name = os.environ.get("MONGO_DB_NAME")
 collection_name_finance = "financial"
 collection_name_reddit = "reddit"
 
-# folder_name = str(date.today())
-
+REF_STRING = "mongodb+srv://admin:<password>@msds697-cluster.qzgwq.mongodb.net/"
+MONGO_DB_NAME = "msds697_project"
+        
 # variables associated with reddit call
 yesterday = datetime.today() - timedelta(days=1)
+yesterday_str = yesterday.strftime("%Y-%m-%d")
 client_secret = 'klhppqo7oLptclnKbyYxcf5ZaSWnYQ'
 client_id = 'NshKC-eP4HnQuF_wNzQNGA'
 user_agent='Stocks'
-list_of_reddits = ['wallstreetbets','stocks','pennystocks','DueDiligence','smallstreetbets','shroomstocks','traders',\
-                   'SPACs','ValueInvesting','UndervaluedStonks','investing','SecurityAnalysis',\
-                   'Wallstreetbetsnew','UnderValuedStocks','EducatedInvesting','RichTogether',\
-                   'greeninvestor','stonks','investing_discussion','options','StockMarket','InvestmentClub',\
-                   'RobinHood']
+# list_of_reddits = ['wallstreetbets','stocks','pennystocks','DueDiligence','smallstreetbets','shroomstocks','traders',\
+#                    'SPACs','ValueInvesting','UndervaluedStonks','investing','SecurityAnalysis',\
+#                    'Wallstreetbetsnew','UnderValuedStocks','EducatedInvesting','RichTogether',\
+#                    'greeninvestor','stonks','investing_discussion','options','StockMarket','InvestmentClub',\
+#                    'RobinHood']
 
-# list_of_reddits = ['shroomstocks']
-
-# # two_days_ago = date.today() - timedelta(days=4)
-# # three_days_ago = date.today() - timedelta(days=5) 
-# two_days_ago = '2023-01-26'
-# three_days_ago = '2023-01-25'
-
-# sf_data_url = "data.sfgov.org"
-# data_limit = 200000
-# sf_data_sub_uri = "gnap-fj3t"
-# sf_data_app_token = os.environ.get("SF_DATA_TOKEN")
-
-# noaa_token = os.environ.get("NOAA_TOKEN")
-# # station_id = "GHCND:USW00023272"
-# # dataset_id = "GHCND"
-# # location_id = "CITY:US060031"
-# noaa_endpoint = f"data?datasetid={dataset_id}&datatypeid=PRCP&station_id={station_id}&startdate={three_days_ago}&enddate={two_days_ago}"
-# noaa_api_url = f"https://www.ncei.noaa.gov/cdo-web/api/v2/{noaa_endpoint}"
-
-# bucket_name = os.environ.get("GS_BUCKET_NAME")
-# service_account_key_file = os.environ.get("GS_SERVICE_ACCOUNT_KEY_FILE")
+list_of_reddits = ['wallstreetbets'] #TEST
 
--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This is a group project part of which goes on towards satisfying requirements fo
 Please note that this is a private repository as of now and access to this repo is limited to the group members and the course instructor and grader. 
 
 ## Team Members
+- Gurusankar Gopalakrishnan
 - Devendra Govil
 - Maneel Reddy
-- Gurusankar Gopalakrishnan
 - Akshay Pamnani
 - Youshi Zhang
 
@@ -25,9 +25,6 @@ Please note that this is a private repository as of now and access to this repo 
 
 
 ## Citation
-
-- This repo uses class material given by Dr. Diane Woodbridge as part of the MSDS 697 course (part of USFCA MSDS curriculum) for setting up base function utilities as well as environment yaml files.
-
 - This repo (as of this current state) relies heavily on the edgar api crawler repo available at this location: https://github.com/nlpaueb/edgar-crawler . The base of this repo was formed using this repo available at: https://github.com/nlpaueb/edgar-crawler
 
 
